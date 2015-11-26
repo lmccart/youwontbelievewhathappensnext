21cf0b71f10ff2991a4b12d6078d5dd22eb92561
Lauren McCarthy <laurmccarthy@gmail.com>
2015-11-26 09:24:54 -0500

    init

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>kyle and lauren merge</title>
+    <script type='text/javascript' src='jquery.js'></script>
+    <!-- bootstrap -->
+    <link rel='stylesheet' type='text/css' href='bootstrap.min.css'>
+    <script type='text/javascript' src='bootstrap.min.js'></script>
+    
+    <script type='text/javascript' src='main.js'></script>
+  </head>
+  <body>
+  <?php include('menu.php'); ?>
+
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include('header.php'); ?>
+
+hi
+
+<?php include('footer.php'); ?>
