e58901cb072f8a4cf4d5e8390c6a7eb3a10e1e09
Lauren McCarthy <laurmccarthy@gmail.com>
2016-02-26 09:32:39 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,5 +4,5 @@
   <!--<li role="presentation" class='menuitem' id='go_weddingparty'><a href="#">WEDDING PARTY</a></li>-->
   <li role="presentation" class='menuitem' id='go_info'><a href="#">INFO</a></li>
   <li role="presentation" class='menuitem' id='go_schedule'><a href="#">SCHEDULE</a></li>
-  <li role="presentation"><a href="https://www.amazon.com/registry/wedding/25RNY2T8INVY5">REGISTRY</a></li>
+  <!--<li role="presentation"><a href="https://www.amazon.com/registry/wedding/25RNY2T8INVY5">REGISTRY</a></li>-->
 </ul>
