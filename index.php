ca02e9b916732f544bb80b9bc9c2f5e0e78080fd
Lauren McCarthy <laurmccarthy@gmail.com>
2016-03-21 22:02:59 -0400

    todo

--- index.php
+++ index.php
@@ -148,9 +148,63 @@
     <b>ART</b>
     <ul>
     <li>
-      <a href="http://www.melafoundation.org/dream02.htm">Dream House</a>
+      <a href="http://www.melafoundation.org/dream02.htm" target="_blank">Dream House</a> – chill out in this light and sound installation in Soho, read more about it <a href="http://www.villagevoice.com/arts/scape-from-new-york-the-legendary-dream-house-installation-finds-a-second-home-in-chelsea-7535028" target='_blank'>here</a>.
       <img src="imgs/dream.jpg"/>
-      Chill out in this light and sound installation in Soho, read more about it <a href="http://www.villagevoice.com/arts/scape-from-new-york-the-legendary-dream-house-installation-finds-a-second-home-in-chelsea-7535028" target='_blank'>here</a>.
+    </li>
+    <li>The <a href="http://guggenheim.org/" target="_blank">Guggenheim Museum</a> has a nice <a href="http://www.guggenheim.org/new-york/exhibitions/upcoming/moholy-nagy-future-present" target="_blank">Moholy-Nagy</a> exhibition on view.</li>
+    <li>
+      <a href="http://www.melafoundation.org/dream02.htm" target="_blank">Whitney</a> Museum of American Art – that rooftop...
+      <img src="imgs/whitney.jpg"/>
+    </li>
+    <li><a href="http://momaps1.org" target="_blank">MoMA PS1</a> (Long Island City)</li>
+    <li><a href="http://www.metmuseum.org/visit/met-cloisters" target="_blank">The Met Cloisters</a> – Travel way up here to 190th St and you will forget you are in NYC for a minute.
+    <img src="imgs/cloisters.jpg"/>
+    </ul>
+    </p>
+  </div>
+
+  <div class='block'>
+    <b>ENTERTAINMENT</b>
+    <ul>
+    <li>
+      <a href="http://www.fatcatmusic.org/" target="_blank">Fat Cat</a> – Live music every night, pool, pong, foosball, shuffle, chess and more.
+      <img src="imgs/fatcat.jpg"/>
+    </li>
+    <li>
+      <a href="http://creeklic.com/" target="_blank">Creek and the Cave</a> – Free comedy every day, and Mexican food.
+      <img src="imgs/creek.jpg"/>
+    </li>
+    <li>
+      <a href="http://www.coneyisland.com/tourist-information" target="_blank">Coney Island</a>! – Making a comeback after Hurricane Sandy. Rides the rides and eat an original <a href="https://en.wikipedia.org/wiki/Nathan%27s_Famous" target="_blank">Nathan's Famous Hotdog</a>
+      <img src="imgs/coneyisland.jpg"/>
+    </li>
+    <li>
+      <a href="http://www.strandbooks.com/" target="_blank">Strand Books</a> – It’s great. It’s been around since 1927.
+      <img src="imgs/strand.jpg"/>
+    </li>
+    <li><a href="http://morbidanatomymuseum.org/" target="_blank">Morbid Anatomy Museum</a> - dedicated to exploring the intersections of death, beauty, and that which falls through the cracks. (Brooklyn)
+    <img src="imgs/morbid.jpg"/>
+    </li>
+    <li><a href="http://www.timessquarenyc.org/index.aspx" target="_blank">Times Square</a> - We can’t really say it’s one of our favorite places to visit, but the tourist in you will be deeply satiated. If you’re there, stand on the grating over the subway ventilation shaft on the pedestrian island where Broadway and Seventh Ave intersect south of 46th St to experience an experimental <a href="http://www.nytimes.com/2006/03/26/nyregion/26sound.html" target="_blank">sound art piece by Max Neuhaus</a>.
+    </ul>
+    </p>
+  </div>
+
+
+  <div class='block'>
+    <b>EAT</b>
+    <ul>
+    <li>
+      <a href="http://www.ippudony.com/" target="_blank">Ippudo</a> – Japanese ramen noodles in the East Village.
+      <img src="imgs/ippudo.jpg"/>
+    </li>
+<!--     <li>
+      <a href="http://www.caracasarepabar.com/index_2.php" target="_blank">Caracas</a> – Arepa bar in the East Village.
+      <img src="imgs/caracas.jpg"/>
+    </li> -->
+    <li>
+      <a href="http://anchoinyc.com/" target="_blank">An Choi</a> – Vietnamese food in the East Village, Bhan Mi, Pho noodles, cocktails.
+      <img src="imgs/anchoi.jpg"/>
     </li>
     </ul>
     </p>
