b36f02fab0caacbd9613e517d12283bac3a20404
Lauren McCarthy <laurmccarthy@gmail.com>
2016-03-21 22:09:30 -0400

    rearrange

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
   <li role="presentation" class="menuitem" id='go_rsvp'><a href="#">RSVP</a></li>
   <!--<li role="presentation" class='menuitem' id='go_weddingparty'><a href="#">WEDDING PARTY</a></li>-->
   <li role="presentation" class='menuitem' id='go_info'><a href="#">INFO</a></li>
-  <li role="presentation" class='menuitem' id='go_todo'><a href="#">@TODO</a></li>
+  <li role="presentation" class='menuitem' id='go_todo'><a href="./todo">@TODO</a></li>
   <li role="presentation" class='menuitem' id="go_merge"><a href='#'>MERGE</a></li>
   <li role="presentation"><a href="https://www.amazon.com/registry/wedding/25RNY2T8INVY5">REGISTRY</a></li>
 </ul>
---
+++ todo/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="icon" type="image/png" href="favicon2.png">
+    <title>K+L</title>
+    <script type='text/javascript' src='../jquery.js'></script>
+    <!-- bootstrap -->
+    <link rel='stylesheet' type='text/css' href='../bootstrap.min.css'>
+    <script type='text/javascript' src='../bootstrap.min.js'></script>
+    <!-- typekit -->
+    <script src="https://use.typekit.net/vuy0rhh.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    <!-- youtube -->
+    <script src="https://www.youtube.com/player_api"></script>
+    
+    <link rel='stylesheet' type='text/css' href='../style.css'>
+  </head>
+  <body>
+  <div id='container'>
+
+  <ul id='menu' class="nav nav-tabs">
+    <li role="presentation" class="menuitem" id='go_home'><a href="../">K+L</a></li>
+    <li role="presentation" class="menuitem" id='go_rsvp'><a href="../#rsvp">RSVP</a></li>
+    <!--<li role="presentation" class='menuitem' id='go_weddingparty'><a href="#">WEDDING PARTY</a></li>-->
+    <li role="presentation" class='menuitem' id='go_info'><a href="../#info">INFO</a></li>
+    <li role="presentation" class='menuitem active' id='go_todo'><a href="./">@TODO</a></li>
+    <li role="presentation" class='menuitem' id="go_merge"><a href='../#merge'>MERGE</a></li>
+    <li role="presentation"><a href="https://www.amazon.com/registry/wedding/25RNY2T8INVY5">REGISTRY</a></li>
+  </ul>
+
+
+
+  <div class='section' id='section_todo' style='display:block'>
+    <p>Kyle and Lauren's favorite spots to visit in NYC and beyond.<br><br></p>
+    <div class='block'>
+      <b>ART</b>
+      <ul>
+      <li>
+        <a href="http://www.melafoundation.org/dream02.htm" target="_blank">Dream House</a> – chill out in this light and sound installation in Soho, read more about it <a href="http://www.villagevoice.com/arts/scape-from-new-york-the-legendary-dream-house-installation-finds-a-second-home-in-chelsea-7535028" target='_blank'>here</a>.
+        <img src="../imgs/dream.jpg"/>
+      </li>
+      <li>The <a href="http://guggenheim.org/" target="_blank">Guggenheim Museum</a> has a nice <a href="http://www.guggenheim.org/new-york/exhibitions/upcoming/moholy-nagy-future-present" target="_blank">Moholy-Nagy</a> exhibition on view.</li>
+      <li>
+        <a href="http://www.melafoundation.org/dream02.htm" target="_blank">Whitney</a> Museum of American Art – that rooftop...
+        <img src="../imgs/whitney.jpg"/>
+      </li>
+      <li><a href="http://momaps1.org" target="_blank">MoMA PS1</a> (Long Island City)</li>
+      <li><a href="http://www.metmuseum.org/visit/met-cloisters" target="_blank">The Met Cloisters</a> – Travel way up here to 190th St and you will forget you are in NYC for a minute.
+      <img src="../imgs/cloisters.jpg"/>
+      </ul>
+      </p>
+    </div>
+
+    <div class='block'>
+      <b>ENTERTAINMENT</b>
+      <ul>
+      <li>
+        <a href="http://www.fatcatmusic.org/" target="_blank">Fat Cat</a> – Live music every night, pool, pong, foosball, shuffle, chess and more.
+        <img src="../imgs/fatcat.jpg"/>
+      </li>
+      <li>
+        <a href="http://creeklic.com/" target="_blank">Creek and the Cave</a> – Free comedy every day, and Mexican food.
+        <img src="../imgs/creek.jpg"/>
+      </li>
+      <li>
+        <a href="http://www.coneyisland.com/tourist-information" target="_blank">Coney Island</a>! – Making a comeback after Hurricane Sandy. Rides the rides and eat an original <a href="https://en.wikipedia.org/wiki/Nathan%27s_Famous" target="_blank">Nathan's Famous Hotdog</a>
+        <img src="../imgs/coneyisland.jpg"/>
+      </li>
+      <li>
+        <a href="http://www.strandbooks.com/" target="_blank">Strand Books</a> – It’s great. It’s been around since 1927.
+        <img src="../imgs/strand.jpg"/>
+      </li>
+      <li><a href="http://morbidanatomymuseum.org/" target="_blank">Morbid Anatomy Museum</a> - dedicated to exploring the intersections of death, beauty, and that which falls through the cracks. (Brooklyn)
+      <img src="../imgs/morbid.jpg"/>
+      </li>
+      <li><a href="http://www.timessquarenyc.org/index.aspx" target="_blank">Times Square</a> - We can’t really say it’s one of our favorite places to visit, but the tourist in you will be deeply satiated. If you’re there, stand on the grating over the subway ventilation shaft on the pedestrian island where Broadway and Seventh Ave intersect south of 46th St to experience an experimental <a href="http://www.nytimes.com/2006/03/26/nyregion/26sound.html" target="_blank">sound art piece by Max Neuhaus</a>.
+      </ul>
+      </p>
+    </div>
+
+
+    <div class='block'>
+      <b>EAT</b>
+      <ul>
+      <li>
+        <a href="http://www.ippudony.com/" target="_blank">Ippudo</a> – Japanese ramen noodles in the East Village.
+        <img src="../imgs/ippudo.jpg"/>
+      </li>
+  <!--     <li>
+        <a href="http://www.caracasarepabar.com/index_2.php" target="_blank">Caracas</a> – Arepa bar in the East Village.
+        <img src="../imgs/caracas.jpg"/>
+      </li> -->
+      <li>
+        <a href="http://anchoinyc.com/" target="_blank">An Choi</a> – Vietnamese food in the East Village, Bhan Mi, Pho noodles, cocktails.
+        <img src="../imgs/anchoi.jpg"/>
+      </li>
+      </ul>
+      </p>
+    </div>
+
+
+  </div>
+</div>
+</body>
+</html>
