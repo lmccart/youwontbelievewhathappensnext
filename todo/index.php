6dad4abf6100be7bbff16cc44c6c68de9e001aac
Lauren McCarthy <laurmccarthy@gmail.com>
2016-04-30 09:09:47 -0400

    Update index.php

--- todo/index.php
+++ todo/index.php
@@ -50,6 +50,10 @@
         <a href="http://www.diaart.org/sites/page/52/1367">Broken Kilometer</a> – One kilometer of solid brass rod, broken. 
         <img src="../imgs/brokenkilometer.png"/>
       </li>
+      <li>
+        <a href="http://www.americandance.org/performance/yvonne-rainer-2/">The Concept of Dust</a> – Yvonne Rainer performance at <a href='http://thekitchen.org/' target='_blank'>The Kitchen</a>, running June 2-4. 
+        <img src="../imgs/dust.png"/>
+      </li>
       <li>The <a href="http://guggenheim.org/" target="_blank">Guggenheim Museum</a> has a nice <a href="http://www.guggenheim.org/new-york/exhibitions/upcoming/moholy-nagy-future-present" target="_blank">Moholy-Nagy</a> exhibition on view.</li>
       <li>
         <a href="http://www.melafoundation.org/dream02.htm" target="_blank">Whitney</a> Museum of American Art – that rooftop...
